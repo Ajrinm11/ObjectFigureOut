21b74df28cfc591a84bb0676a2bbeeeee25d9f94
Ajrinm11 <84498693+Ajrinm11@users.noreply.github.com>
2022-12-06 18:45:02 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    document.getElementById("shutter").play();
+
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CuB0wm10g/model.json',modelLoaded);
+
+function modelLoaded()
+{
+console.log("Model is loaded, and Ajrin is very happy! :)");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+    document.getElementById("compare").play();
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+    }
